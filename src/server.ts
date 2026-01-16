63a59ea6a4130ccc0c7b2e8357ebee45a57aaa85
MikeD <isofarro2@gmail.com>
2026-01-16 22:00:48 +0000

    limit server to 127.0.0.1

--- src/server.ts
+++ src/server.ts
@@ -8,7 +8,7 @@ const app = Fastify({ logger: true });
 app.register(gameRoutes, { prefix: '/games' });
 
 const { port } = getConfig();
-app.listen({ port, host: '0.0.0.0' }, (err) => {
+app.listen({ port, host: '127.0.0.1' }, (err) => {
   if (err) {
     app.log.error(err);
     process.exit(1);
